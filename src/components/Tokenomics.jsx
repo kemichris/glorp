89c31669704ecf1d5df79cc6b6143b662e6655fe
kemi Chris <ekeminichristopher123@gmail.com>
2024-07-17 01:31:03 +0100

    tokenomics

--- src/components/Tokenomics.jsx
+++ src/components/Tokenomics.jsx
@@ -5,29 +5,29 @@ import TokenomicsImg from "../assets/glorp3-copy.png"
 export const Tokenomics = () => {
 
     const copyAddress = ()=>{
-        navigator.clipboard.writeText("......React.")
+        navigator.clipboard.writeText("Contract address")
         // enqueueSnackbar("Address copied", {variant: "success"});
     }
 
   return (
-    <div>
+    <div className='tokenomics'>
         <h2>Tokenomics</h2>
         <div className="tokenomicsDetails">
             <img src={TokenomicsImg} alt="" />
             <div className="tokenomicsText">
-                <p>$GLRP is Happy</p>
-                <p>Token supply</p>
-                <h3>10000000</h3>
+                <p className='ticker'>$GLRP is Happy</p>
+                <p className='tokenSupply'>Token supply</p>
+                <h3 className='tokenSupplyFigure'>1000,000,000</h3>
                 <ul>
-                    <li>90% liquidity pool</li>
-                    <li>5% NFT Airdrop = 3.75% Coolman's Universe | 1.25% Babies</li>
-                    <li>5% for future rewards coming soon</li>
+                    <li><strong>90</strong>% liquidity pool</li>
+                    <li><strong>5</strong>% NFT Airdrop = 3.75% Coolman's Universe | 1.25% Babies</li>
+                    <li><strong>5</strong>% for future rewards coming soon</li>
                 </ul>
 
                 <div className="contractAddress">
-                    <p>Token Address</p>
-                    <p onClick={copyAddress}>........</p>
-                    <i class="fa-solid fa-copy"></i>
+                    <p className='caText'>Contract Address</p>
+                    <p className='ca' onClick={copyAddress}>akfjklasjflkasjkdfasdjkfjsakfjdlksdjfklds <i class="fa-solid fa-copy"></i></p>
+                    
                 </div>
             </div>
         </div>
