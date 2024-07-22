1a1c37e5e13c2df84381c8e330f9ed59356ab629
kemi Chris <ekeminichristopher123@gmail.com>
2024-07-22 10:12:31 +0100

    mobile responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import Logo from "../assets/glorp_logo.png";
 
 
 export const Navbar = () => {
 
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
 
-  const toggleDropdown = ()=> {
+ const mobileMenuRef = useRef(null);
+
+  const toggleDropdown = () => {
     setIsOpen(!isOpen)
   }
+
+const toggleMenu = () => {
+  mobileMenuRef.current.classList.toggle("active")
+  
+}
 
   return (
     <div className='navbar'>
@@ -23,15 +30,26 @@ export const Navbar = () => {
         </div>
         <button className='socialsBtn' onClick={toggleDropdown}>Socials <i class="fa-solid fa-caret-down"></i></button>
         {isOpen && (
-         <div className="socialDropdown">
-          <a href="https://x.com/GLRPonSol" target="_blank" rel="noopener noreferrer">
-          <i class="fa-brands fa-x-twitter"></i>
-          X(Twitter)
-          </a>
-        </div>
-      )}
-       
+          <div className="socialDropdown">
+
+          </div>
+
+        )}
+        <i class="fa-solid fa-chart-bar navBtn" onClick={toggleMenu}></i>
+
       </nav>
+      <div className="mobileMenu" ref={mobileMenuRef}>
+        <div className='closeMenu'>
+          <i class="fa-solid fa-xmark"></i>
+        </div>
+        <a href="http://localhost:3000">Home</a>
+        <a href="#about">About</a>
+        <a href="#tokenomics">Tokenomics</a>
+        <a href="#buy">How to buy</a>
+        <div className="menuSocials">
+          <a href="https://x.com/GLRPonSol" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-x-twitter"></i></a>
+        </div>
+      </div>
     </div>
   );
 };
