084f863b7092ccfcd57430f9ef696579dd8657c1
kemi Chris <ekeminichristopher123@gmail.com>
2024-07-24 08:12:53 +0100

    loading complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 
+import { useEffect, useState } from 'react';
 import './App.css';
 import { About } from './components/About';
 import { Footer } from './components/Footer';
-// import { Loading } from './components/Loading';
+import { Loading } from './components/Loading';
 import { MarqueeBox } from './components/MarqueeBox';
 import { Navbar } from './components/Navbar';
 import { Roadmap } from './components/Roadmap';
@@ -10,13 +11,51 @@ import { Tokenomics } from './components/Tokenomics';
 import { Topcontainer } from './components/Topcontainer';
 
 function App() {
+
+  const [mediaLoaded, setMediaLoaded] = useState(false);
+
+  useEffect( ()=> {
+    const images = document.querySelectorAll("img");
+    const videos = document.querySelectorAll("video");
+    const totalMedia = images.length + videos.length;
+
+    let loadedCount = 0;
+
+    images.forEach((img) => {
+      img.onload = () => {
+        loadedCount++;
+        if (loadedCount === totalMedia) {
+          setMediaLoaded(true);
+          
+        }
+      }
+    })
+
+    videos.forEach((video) => {
+      video.onload = () => {
+        loadedCount++;
+        if (loadedCount === totalMedia) {
+          setMediaLoaded(true);
+          
+        }
+      }
+    })
+
+    if (!mediaLoaded) {
+      const loadingDiv = document.querySelector(".loading");
+
+      loadingDiv.style.display ="none";
+      
+    }
+
+  })
   return (
     <div className="App">
       <header>
         <Navbar />
       </header>
       <main>
-        {/* <Loading /> */}
+        <Loading />
         <Topcontainer />
         <MarqueeBox/>
         <About />
